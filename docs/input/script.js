f622e2af0cb5b7a6a94d81e254a223a8a4b2b7eb
Gabriel Grossman <gagrossman@packer.edu>
2018-04-13 14:03:41 -0400

    graduation

--- docs/input/script.js
+++ docs/input/script.js
@@ -1,9 +1,43 @@
-var button = document.getElementById('b1');
-button.addEventListener("click", buttonPress);
+var button1 = document.getElementById('b1');
+button1.addEventListener("click", buttonPress1);
 
-function buttonPress() {
+var button2 = document.getElementById('b2');
+button2.addEventListener("click", buttonPress2);
+
+var button3 = document.getElementById('b3');
+button3.addEventListener("click", buttonPress3);
+
+var button4 = document.getElementById('b4');
+button4.addEventListener("click", buttonPress4);
+
+function buttonPress1() {
+	console.log("buttonPress1");
 	var val1  = document.getElementById("input1").value;
 	var val2 = document.getElementById("input2").value;
-	var sum = (val1*1+val2*1);
-	document.getElementById("result").innerHTML = sum;
+	var result = (val1*1+val2*1);
+	document.getElementById("result").innerHTML = result;
+}
+
+function buttonPress2() {
+	console.log("buttonPress2");
+	var val1  = document.getElementById("input1").value;
+	var val2 = document.getElementById("input2").value;
+	var result = (val1*1-val2*1);
+	document.getElementById("result").innerHTML = result;
+}
+
+function buttonPress3() {
+	console.log("buttonPress3");
+	var val1  = document.getElementById("input1").value;
+	var val2 = document.getElementById("input2").value;
+	var result = (val1*1*val2*1);
+	document.getElementById("result").innerHTML = result;
+}
+
+function buttonPress4() {
+	console.log("buttonPress4");
+	var val1  = document.getElementById("input1").value;
+	var val2 = document.getElementById("input2").value;
+	var result = ((val1*1)/(val2*1));
+	document.getElementById("result").innerHTML = result;
 }
---
+++ docs/objects/script.js
@@ -0,0 +1,14 @@
+var graduation = new Date(2018, 5, 14).getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = graduation - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+}, 1000);
